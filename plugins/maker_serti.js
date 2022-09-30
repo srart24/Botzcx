c7fe9464487f703bd625f4f329cc77033057ef5a
srart24 <86997468+srart24@users.noreply.github.com>
2022-09-30 10:00:00 +0700

    Update maker_serti.js

--- plugins/maker_serti.js
+++ plugins/maker_serti.js
@@ -5,7 +5,7 @@ if (!text) return m.reply(`*Contoh : ${usedPrefix}${command} ${bnm}*`)
 let res = await API('sr', `/sertifikat/${command}`, { text: text }, 'apikey')
   conn.sendHydrated(m.chat, null , null, res, lnkweb, nmweb, null, null, [ [], ], m)
 	}
-  handler.help = ['tololserti', 'wibuserti', 'goodlookingserti', 'mlserti', 'epepserti', 'fuckboyserti', 'fuckgirlserti', 'bucinswrti'].map(v => v + ' <Nama>')
+  handler.help = ['tololserti', 'wibuserti', 'goodlookingserti', 'mlserti', 'epepserti', 'fuckboyserti', 'fuckgirlserti', 'bucinserti'].map(v => v + ' <Nama>')
   handler.tags = ['sertifikat'] 
   handler.command = ['tololserti', 'wibuserti', 'goodlookingserti', 'mlserti', 'epepserti', 'fuckboyserti', 'fuckgirlserti', 'bucinserti']
   handler.limit = true
